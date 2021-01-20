f77023dbc7ff7b20208c2cc1620390eee667e30f
woojaang <woojaang@naver.com>
2021-01-20 09:57:46 +0900

    update

--- test_text_processing.py
+++ test_text_processing.py
@@ -2,7 +2,7 @@
 
 import unittest
 import random
-import test_processing as tp
+import text_processing as tp
 
 
 class TestTextProcessing(unittest.TestCase):
@@ -91,4 +91,3 @@ class TestTextProcessing(unittest.TestCase):
         test_str = "abcdefghijklmnopqrstuvwxyz"
         pred = tp.no_vowels(test_str)
         self.assertEqual(pred, "bcdfghjklmnpqrstvwxyz")
-        
